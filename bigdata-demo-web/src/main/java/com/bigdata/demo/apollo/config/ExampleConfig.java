e260ee2f23f30a9f34e71417a9996ef452370974
Zeron <zengzhirong87@163.com>
2021-06-10 17:57:39 +0800

    update .gitignore

--- bigdata-demo-web/src/main/java/com/bigdata/demo/apollo/config/ExampleConfig.java
+++ bigdata-demo-web/src/main/java/com/bigdata/demo/apollo/config/ExampleConfig.java
@@ -2,19 +2,39 @@ package com.bigdata.demo.apollo.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfigChangeListener;
+
 import lombok.Data;
 
 @Data
 @ConfigurationProperties(prefix = "example")
 public class ExampleConfig {
-	
+
 	private int expireSeconds;
-	
+
 	private int commandTimeout;
-	
+
+	@ApolloConfig("example")
+	private Config config;
+
 	@Override
 	public String toString() {
 		return "ExpireSeconds: " + expireSeconds + ", CommandTimeout: " + commandTimeout;
 	}
 
+	@ApolloConfigChangeListener("example")
+	private void onChange(ConfigChangeEvent event) {
+		System.out.println("======event change=====");
+		for(String key : event.changedKeys()) {
+			if(key.equals("example.expireSeconds")) {
+				this.expireSeconds = config.getIntProperty("example.expireSeconds", 0);
+			} else if(key.equals("example.commandTimeout")) {
+				this.commandTimeout = config.getIntProperty("example.commandTimeout", 0);
+			}
+			System.out.println("key:" + key);
+		}
+	}
 }
